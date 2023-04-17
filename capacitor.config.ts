6e9a14f190fe058d0ec4dfe436ba33e611c56b65
Closus <claudio@manca.be>
2023-04-17 16:28:44 +0200

    [FIX] Add logo and name and make white logo in log

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'atkApp',
+  appName: 'Auto Tracking Mobile',
   webDir: 'www',
   bundledWebRuntime: false
 };
